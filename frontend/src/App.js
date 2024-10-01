2aa231d213ae722de09b36cfb1b6e621c6e8efac
TwittK <koitristan123@gmail.com>
2024-10-01 18:58:58 +0800

    Added fav loan login reg page, put login and reg into navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Navbar';
 import Books from './Books'; 
 import Locations from './Locations';
+import Loans from './Loans';
+import Favourites from './Favourites';
+import Login from './Login';
+import Register from './Register'
 import './App.css';
 
 function App() {
@@ -12,7 +16,11 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Books />} />
-                    <Route path="/locations" element={<Locations />} />
+                    <Route path="/Locations" element={<Locations />} />
+                    <Route path="/Loans" element={<Loans />} />
+                    <Route path="/Favourites" element={<Favourites />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Register" element={<Register />} />
                 </Routes>
             </div>
         </Router>
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/locations">Locations</Link></li>
-                <li><Link to="/loans">Loans</Link></li>
-                <li><Link to="/favourites">Favourites</Link></li>
-            </ul>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/locations">Locations</Link>
+                <Link to="/loans">Loans</Link>
+                <Link to="/favourites">Favourites</Link>
+            </div>
+            <div className="auth-links">
+                <Link to="/login">Log in</Link>
+                <Link to="/register">Register</Link>
+            </div>
         </nav>
     );
 }
